fc09edcbfe57625e7763d5dd9087506f8b468a85
Daniel Schmidt <dschmidt@mesosphere.com>
2019-07-20 20:03:31 +0200

    style: app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 import * as React from "react";
+import { Paper, Container, Box } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
 import FigureTypeahead from "./components/FigureTypeahead";
 import VideoPreviews from "./containers/VideoPreviews";
+
 import "./App.css";
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(3, 2)
+  }
+}));
+
 function FigureSearch() {
+  const classes = useStyles();
   const [dance, setDance] = React.useState<string>("");
   const [figure, setFigure] = React.useState<string>("");
 
   return (
-    <>
+    <Paper className={classes.root}>
       <FigureTypeahead setDance={setDance} setFigure={setFigure} />
       <VideoPreviews dance={dance} figure={figure} />
-    </>
+    </Paper>
   );
 }
 
@@ -21,9 +32,11 @@ function App() {
       <header className="App-header">
         <h1 className="App-title">YouDance</h1>
       </header>
-      <main>
-        <FigureSearch />
-      </main>
+      <Container>
+        <Box m={4}>
+          <FigureSearch />
+        </Box>
+      </Container>
     </div>
   );
 }
