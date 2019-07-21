447d890c4a8ef998face8d6396d7dab5b9a888a4
Daniel Schmidt <dschmidt@mesosphere.com>
2019-07-21 12:06:13 +0200

    feat(editor): add new figure and video view

--- src/pages/Classify.tsx
+++ src/pages/Classify.tsx
@@ -1,9 +1,150 @@
 import * as React from "react";
-import { Box, CircularProgress } from "@material-ui/core";
+import {
+  Box,
+  CircularProgress,
+  Snackbar,
+  SnackbarContent
+} from "@material-ui/core";
 import { match } from "react-router";
-import { useFindDanceQuery } from "../generated/graphql";
+import {
+  useFindDanceQuery,
+  useAddFigureMutation,
+  useAddFigureVideoMutation
+} from "../generated/graphql";
 
 import "./Classify.css";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
+interface Figure {
+  _id: string;
+  name: string;
+}
+
+interface AddFigureFormProps {
+  danceId: string;
+  youtubeId: string;
+  knownFigures: Figure[];
+}
+
+function AddFigureForm({
+  knownFigures,
+  danceId,
+  youtubeId
+}: AddFigureFormProps) {
+  const [addFigure] = useAddFigureMutation();
+  const [addFigureVideo] = useAddFigureVideoMutation();
+  const [showSuccessMessage, showSuccess] = React.useState(false);
+
+  return (
+    <>
+      <Formik
+        initialValues={{ figureSelection: "", newFigure: "", start: 0, end: 0 }}
+        validate={values => {
+          if (!values.figureSelection) {
+            return { figureSelection: "Required" };
+          }
+
+          if (values.figureSelection === "new-figure") {
+            if (!values.newFigure) {
+              return {
+                newFigure: "Required"
+              };
+            }
+          }
+
+          if (!values.start) {
+            return { start: "Required" };
+          }
+          if (!values.end) {
+            return { end: "Required" };
+          }
+        }}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+          setSubmitting(true);
+          resetForm();
+
+          const figureIdPromise =
+            values.figureSelection === "new-figure"
+              ? addFigure({
+                  variables: { dance: danceId, name: values.newFigure }
+                }).then(value => value.data.createFigure._id)
+              : Promise.resolve(values.figureSelection);
+
+          figureIdPromise
+            .then(figureId =>
+              addFigureVideo({
+                variables: {
+                  figureId,
+                  youtubeId,
+                  start: values.start,
+                  end: values.end
+                }
+              })
+            )
+            .then(() => {
+              setSubmitting(false);
+              showSuccess(true);
+            });
+        }}
+      >
+        {form => (
+          <Form>
+            <Field component="select" name="figureSelection">
+              <option value="" />
+              {knownFigures.map(figure => {
+                return (
+                  <option key={figure._id} value={figure._id}>
+                    {figure.name}
+                  </option>
+                );
+              })}
+              <option value="new-figure">Add new Figure</option>
+            </Field>
+            <ErrorMessage name="figureSelection" component="div" />
+
+            {form.values.figureSelection === "new-figure" ? (
+              <>
+                <Field name="newFigure" />
+                <ErrorMessage name="newFigure" component="div" />
+              </>
+            ) : null}
+
+            <span>Start (in s)</span>
+            <Field type="number" name="start" />
+            <ErrorMessage name="start" component="div" />
+
+            <span>End (in s)</span>
+            <Field type="number" name="end" />
+            <ErrorMessage name="end" component="div" />
+
+            <button type="submit" disabled={form.isSubmitting || !form.isValid}>
+              Add Figure
+            </button>
+
+            {form.isSubmitting ? <CircularProgress /> : null}
+          </Form>
+        )}
+      </Formik>
+
+      <Snackbar
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left"
+        }}
+        open={showSuccessMessage}
+        autoHideDuration={6000}
+        onClose={() => showSuccess(false)}
+      >
+        <SnackbarContent
+          aria-describedby="add-success"
+          message={
+            <span id="add-success">Successfully added a new Figure</span>
+          }
+        />
+      </Snackbar>
+    </>
+  );
+}
 
 async function getYoutubeVideoId(danceName: string) {
   const searchTerm = encodeURIComponent(`wdsf ${danceName}`);
@@ -32,7 +173,7 @@ interface VideoClassificationProps {
   danceId: string;
 }
 
-function VideoClassification({ danceName }: VideoClassificationProps) {
+function VideoClassification({ danceName, danceId }: VideoClassificationProps) {
   const [youtubeId, setYoutubId] = React.useState();
   // Search video on youtube
 
@@ -45,10 +186,17 @@ function VideoClassification({ danceName }: VideoClassificationProps) {
   }
 
   return (
-    <iframe
-      className="Classify-review"
-      src={"https://www.youtube.com/embed/" + youtubeId}
-    />
+    <>
+      <iframe
+        className="Classify-review"
+        src={"https://www.youtube.com/embed/" + youtubeId}
+      />
+      <AddFigureForm
+        danceId={danceId}
+        youtubeId={youtubeId}
+        knownFigures={[]}
+      />
+    </>
   );
 }
 
